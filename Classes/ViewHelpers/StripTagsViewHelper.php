898189cf0eacdbf53ee093cdb06abc93904da5d8
Apen <cerdanyohann@yahoo.fr>
2013-05-27 10:15:56 +0200

    Custom strip tags viewhelper

---
+++ Classes/ViewHelpers/StripTagsViewHelper.php
@@ -0,0 +1,64 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Yohann CERDAN <cerdanyohann@yahoo.fr>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * ViewHelper to use striptags function
+ *
+ * Example
+ * <t3devapi:stripTags>{string}</t3devapi:stripTags><
+ *
+ * Renders the striptags
+ *
+ * @package    TYPO3
+ * @subpackage t3devapi
+ */
+class Tx_T3devapi_ViewHelpers_StripTagsViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * Disable the escaping interceptor because otherwise the child nodes would be escaped before this view helper
+	 * can decode the text's entities.
+	 *
+	 * @var boolean
+	 */
+	protected $escapingInterceptorEnabled = FALSE;
+
+	/**
+	 * Escapes special characters with their escaped counterparts as needed using PHPs strip_tags() function.
+	 *
+	 * @param string $value string to format
+	 * @see http://www.php.net/manual/function.strip-tags.php
+	 * @api
+	 */
+	public function render($value = NULL) {
+		if ($value === NULL) {
+			$value = $this->renderChildren();
+		}
+		if (!is_string($value)) {
+			return $value;
+		}
+		return strip_tags($value);
+	}
+}
+
+?>

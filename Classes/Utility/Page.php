d3d5a6979a944b64b8bad98ba7101542ab1297e3
Apen <cerdanyohann@yahoo.fr>
2015-01-27 12:31:14 +0100

    [BUGFIX] Fix to extendPidListByChildren() functio

--- Classes/Utility/Page.php
+++ Classes/Utility/Page.php
@@ -35,37 +35,29 @@
  * @subpackage t3devapi
  */
 class Tx_T3devapi_Utility_Page {
+
     /**
      * Find all ids from given ids and level
      *
-     * @param string  $pidList   comma seperated list of ids
+     * @param string $pidList comma separated list of ids
      * @param integer $recursive recursive levels
-     * @return string comma seperated list of ids
+     * @return string comma separated list of ids
      */
     public static function extendPidListByChildren($pidList = '', $recursive = 0) {
+        $recursive = (int)$recursive;
         if ($recursive <= 0) {
             return $pidList;
         }
-
-        $cObj = t3lib_div::makeInstance('tslib_cObj');
-
-        $recursive = Tx_T3devapi_Utility_Compatibility::forceIntegerInRange($recursive, 0);
-
-        $pidList = array_unique(t3lib_div::trimExplode(',', $pidList, 1));
-
-        $result = array();
-
-        foreach ($pidList as $pid) {
-            $pid = Tx_T3devapi_Utility_Compatibility::forceIntegerInRange($pid, 0);
-            if ($pid) {
-                $children = $cObj->getTreeList(-1 * $pid, $recursive, 0, TRUE);
-                if ($children) {
-                    $result[] = $children;
-                }
+        $queryGenerator = t3lib_div::makeInstance('t3lib_queryGenerator');
+        $recursiveStoragePids = $pidList;
+        $storagePids = t3lib_div::intExplode(',', $pidList);
+        foreach ($storagePids as $startPid) {
+            $pids = $queryGenerator->getTreeList($startPid, $recursive, 0, 'hidden=0');
+            if (strlen($pids) > 0) {
+                $recursiveStoragePids .= ',' . $pids;
             }
         }
-
-        return implode(',', $result);
+        return $recursiveStoragePids;
     }
 }
 
